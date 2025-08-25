67012b8bcad8f3148ca521df93f942405ee71008
git <harishkumar16092006@gmail.com>
2025-08-25 13:00:52 +0530

    New structure and function

--- projects/HARISH_KUMAR/project.c
+++ projects/HARISH_KUMAR/project.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
+#include<time.h>
 struct seat{
     int s_no;
     char from[50];
@@ -9,11 +10,20 @@ struct seat{
     struct seat* next_s;
 };
 struct coach{
+    int coach_no;
     struct seat* seats;
     struct coach* next_c;
 
 };
+struct  haults{
+    char name[50];
+    struct tm arrival;
+    struct tm departure;
+    struct station* next_s;
+};
 struct train{
+    int train_no;
     struct coach* coaches;
 
 };
+struct train* createtrain(int seat,int coaches);
